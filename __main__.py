28dc3ac2f372a57f69dba26411ef69f60e91ce3c
Jonathan Leeming <jpleeming51@gmail.com>
2020-09-26 19:08:57 +0100

    Added text & updated buttons

--- __main__.py
+++ __main__.py
@@ -1,14 +1,18 @@
 from pygame import (
 	init as pg_init,
-	QUIT as pg_QUIT,
+	QUIT as EVENT_QUIT,
 	gfxdraw as draw,
 	draw as _draw,
+	MOUSEBUTTONDOWN as EVENT_MOUSE_BUTTON_DOWN,
 )
 draw.filled_rectangle = lambda surface, rect, colour: _draw.rect(surface, colour, rect)
 from pygame.display import (
 	set_mode as set_size,
 	set_caption as set_title,
 	update as update_display,
+)
+from pygame.font import (
+	Font
 )
 from pygame.time import (
 	Clock,
@@ -21,6 +25,8 @@ from vector import Vector2
 
 
 class App:
+	CENTER = 0
+
 	def __init__(self):
 		pg_init()
 
@@ -36,9 +42,9 @@ class App:
 		self.letters = [
 			{ 'letter': l, 'lit': False }
 			for l in [
-				'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
-				  'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
-				    'Z', 'X', 'C', 'V', 'B', 'N', 'M',
+				'Q', 'W', 'E', 'R', 'T', 'Z', 'U', 'I', 'O',
+				  'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K',
+				'P', 'Y', 'X', 'C', 'V', 'B', 'N', 'M', 'L',
 			]
 		]
 
@@ -48,8 +54,12 @@ class App:
 		self.running = True
 		while self.running:
 			for event in get_events():
-				if event.type == pg_QUIT:
+				t = event.type
+				d = event.dict
+				if t == EVENT_QUIT:
 					self.running = False
+				elif t == EVENT_MOUSE_BUTTON_DOWN:
+					print(Vector2(d['pos']))
 				else:
 					print(event)
 
@@ -60,20 +70,25 @@ class App:
 
 	def render_keyboard(self):
 		self.rect(0xe17055, (0.1, 0.1), (0.8, 0.8))
-		for i in range(10):
-			self.render_button(Vector2(0.14 + i * 0.08, 0.16), i)
 		for i in range(9):
-			self.render_button(Vector2(0.18 + i * 0.08, 0.36), i + 10)
-		for i in range(7):
-			self.render_button(Vector2(0.22 + i * 0.08, 0.56), i + 19)
+			self.render_button(Vector2(0.14 + i * 0.09, 0.16), i)
+		for i in range(8):
+			self.render_button(Vector2(0.185 + i * 0.09, 0.245), i + 9)
+		for i in range(9):
+			self.render_button(Vector2(0.14 + i * 0.09, 0.33), i + 17)
 
 	def render_button(self, pos, idx):
 		x, y = Vector2(pos).tuple(float)
 		l = self.letters[idx]
-		print(x, y, l)
+
+		c = 0xfdcb6e if l['lit'] else 0xdfe6e9
+		
+		self.circle(c, (x, y), 0.04, outline=0xdfe6e9)
+		self.text(l['letter'], ('res/Montserrat-Medium.ttf', 16), 0x2d3436, (x, y))
 	
-		self.circle(0xfefefe, (x, y), 0.04)
-		self.circle(0xfefefe, (x, y + 0.09), 0.04)
+		y += .32
+		self.circle(0x2d3436, (x, y), 0.04, outline=0xdfe6e9)
+		self.text(l['letter'], ('res/Montserrat-Medium.ttf', 24), 0xdfe6e9, (x, y))
 
 	@staticmethod
 	def process_colour(c):
@@ -87,22 +102,40 @@ class App:
 
 	def rect(self, c, pos, size):
 		c = App.process_colour(c)
-		pos = Vector2(pos)
-		size = Vector2(size)
+		pos = Vector2(pos) * self.size
+		size = Vector2(size) * self.size
 
 		draw.filled_rectangle(
 			self.display,
-			( *((pos * self.size).tuple(int)), *((size * self.size).tuple(int)) ),
+			( *(pos.tuple(int)), *(size.tuple(int)) ),
 			c
 		)
 
-	def circle(self, c, pos, r):
+	def circle(self, c, pos, r, outline = 0):
 		c = App.process_colour(c)
+		outline = App.process_colour(outline)
 		pos = Vector2(pos) * self.size
 		r *= min(self.width, self.height)
 
 		draw.filled_circle(self.display, *pos.tuple(int), int(r), c)
-		draw.aacircle(self.display, *pos.tuple(int), int(r), App.process_colour(0))
+		draw.aacircle(self.display, *pos.tuple(int), int(r), outline)
+
+	def text(self, text, f, c, pos, draw_mode = CENTER):
+		f = Font(*f)
+		c = App.process_colour(c)
+		pos = Vector2(pos) * self.size
+
+		text_surface = f.render(text, True, c)
+		text_rect = text_surface.get_rect()
+		
+		if draw_mode == App.CENTER:
+			text_rect.center = pos.tuple(int)
+		else:
+			text_rect.center = (pos + Vector2(text_rect.size) / 2).tuple(int)
+
+		self.display.blit(text_surface, text_rect)
 
 if __name__ == '__main__':
+	from os import system
+	system('cls')
 	App()
