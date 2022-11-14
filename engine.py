c82da66b04d91056103a99684a732445b6fc328e
Jonathan Leeming <jpleeming51@gmail.com>
2022-11-14 23:13:17 +0000

    Progress

--- engine.py
+++ engine.py
@@ -130,6 +130,10 @@ class Engine():
 			self.event_listeners[event] = []
 		self.add_event_listener(EVENT_QUIT, self.on_quit)
 
+		self.translation = Vector2()
+		self.translations = []
+		# self.scale = 1 # TODO: add scale
+
 		self.setup()
 		self()
 
@@ -137,12 +141,14 @@ class Engine():
 		self.running = True
 		while self.running:
 			for event in get_events():
-				for listener in self.event_listeners[event.type]:
-					d = event.dict
-					if 'pos' in event.dict:
-						d['pos'] = Vector2(d['pos'])
-					listener(**d)
+				if event.type in self.event_listeners:
+					for listener in self.event_listeners[event.type]:
+						d = event.dict
+						if 'pos' in event.dict:
+							d['pos'] = Vector2(d['pos'])
+						listener(**d)
 
+			self.display.fill(Colour(0))
 			self.loop()
 
 			update_display()
@@ -160,24 +166,128 @@ class Engine():
 	def on_quit(self):
 		self.running = False
 
+	@staticmethod
+	def get_Colour(kwargs, *args):
+		c = get_first_from_dict(kwargs, *args)
+		c = Colour(c) if c is not None else None
+		return c
+
+	@staticmethod
+	def get_Vector2(kwargs, *args):
+		v = get_first_from_dict(kwargs, *args)
+		v = Vector2(v) if v is not None else None
+		return v
+
+	@staticmethod
+	def get_Font(kwargs, *args):
+		f = get_first_from_dict(kwargs, *args)
+		f = Font(f) if f is not None else None
+		return f
+
+	@staticmethod
+	def get_c(kwargs):
+		c = Engine.get_Colour(kwargs, 'c', 'col', 'color', 'colour')
+		return c
+
+	@staticmethod
+	def get_outline(kwargs):
+		c = Engine.get_Colour(kwargs, 'outline')
+		c = c if c is not None else Engine.get_c(kwargs)
+		return c
+
+	@staticmethod
+	def get_background(kwargs):
+		c = Engine.get_Colour(kwargs, 'bg', 'background')
+		return c
+
+	@staticmethod
+	def get_bg(kwargs):
+		return Engine.get_background(kwargs)
+
+	def get_pos(self, kwargs):
+		pos = Engine.get_Vector2(kwargs, 'pos')
+		return pos + self.translation
+
+	@staticmethod
+	def get_size(kwargs):
+		size = Engine.get_Vector2(kwargs, 'size')
+		return size
+
+	@staticmethod
+	def get_scaled(kwargs):
+		scaled = get_first_from_dict(kwargs, 'scaled', default=False)
+		return scaled
+
+	@staticmethod
+	def get_font(kwargs):
+		f = Engine.get_Font(kwargs, 'f', 'font')
+		return f
+
+	@staticmethod
+	def get_alignment(kwargs):
+		horiz_align = get_from_dict(kwargs, 'horiz_align', default=Engine.CENTERh)
+		vert_align = get_from_dict(kwargs, 'vert_align', default=Engine.CENTERv)
+		align = get_from_dict(kwargs, 'align', default=horiz_align | vert_align)
+		return align
+
+	def __scale(self, *args, scaled = False):
+		s = self.size if not scaled else 1
+		if len(args) == 1:
+			return args[0] * s
+		return [ a * s for a in args ]
+
+	@staticmethod
+	def not_None(**args):
+		nones = [ k for k, v in args.items() if v is None ]
+		if len(nones) > 0:
+			raise ValueError('Must provide values for ' + str(nones)[1:-1].replace('\'', ''))
+
+	@staticmethod
+	def align(size, alignment):
+		align = alignment
+		s = Vector2(size)
+		w, h = s.tuple()
+		pos = Vector2()
+
+		if align & Engine.TOP != 0:
+			pos += (0, h/2)
+		elif align & Engine.CENTERv != 0:
+			pos -= (0, 0)
+		elif align & Engine.BOTTOM != 0:
+			pos -= (0, h/2)
+
+		if align & Engine.LEFT != 0:
+			pos += (w/2, 0)
+		elif align & Engine.CENTERh != 0:
+			pos -= (0, 0)
+		elif align & Engine.RIGHT != 0:
+			pos -= (w/2, 0)
+
+		return pos
+
+	@staticmethod
+	def render_text(text, **kwargs):
+		f = Engine.get_font(kwargs)
+		c = Engine.get_c(kwargs)
+		bg = Engine.get_bg(kwargs)
+
+		Engine.not_None(font=f, colour=c)
+
+		text_surface = f.render(text, True, c, bg)
+		text_rect = text_surface.get_rect()
+		return text_surface, text_rect
+
+
 	def filled_rectangle(self, **kwargs):
-		c = get_first_from_dict(kwargs, 'c', 'col', 'colour')
-		pos = get_from_dict(kwargs, 'pos')
-		size = get_from_dict(kwargs, 'size')
-		outline = get_from_dict(kwargs, 'outline')
-		scaled = get_from_dict(kwargs, 'scaled', default = False)
+		c = self.get_c(kwargs)
+		pos = self.get_pos(kwargs)
+		size = self.get_size(kwargs)
+		outline = self.get_outline(kwargs)
+		scaled = self.get_scaled(kwargs)
 
-		if None in (c, pos, size):
-			raise ValueError('Must provide colour, position and size')
-		
-		c = Colour(c)
-		pos = Vector2(pos)
-		size = Vector2(size)
-		outline = Colour(outline) if outline is not None else c
+		self.not_None(colour=c, position=pos, size=size)
 
-		if not scaled:
-			pos *= self.size
-			size *= self.size
+		pos, size = self.__scale(pos, size, scaled=scaled)
 
 		draw.filled_rectangle(
 			self.display,
@@ -190,21 +300,14 @@ class Engine():
 		)
 
 	def rectangle(self, **kwargs):
-		c = get_first_from_dict(kwargs, 'c', 'col', 'colour')
-		pos = get_from_dict(kwargs, 'pos')
-		size = get_from_dict(kwargs, 'size')
-		scaled = get_from_dict(kwargs, 'scaled', default = False)
+		c = self.get_c(kwargs)
+		pos = self.get_pos(kwargs)
+		size = self.get_size(kwargs)
+		scaled = self.get_scaled(kwargs)
 
-		if None in (c, pos, size):
-			raise ValueError('Must provide colour, position and size')
-		
-		c = Colour(c)
-		pos = Vector2(pos)
-		size = Vector2(size)
+		self.not_None(colour=c, position=pos, size=size)
 
-		if not scaled:
-			pos *= self.size
-			size *= self.size
+		pos, size = self.__scale(pos, size, scaled=scaled)
 
 		draw.rectangle(
 			self.display,
@@ -213,23 +316,16 @@ class Engine():
 		)
 
 	def filled_circle(self, **kwargs):
-		c = get_first_from_dict(kwargs, 'c', 'col', 'colour')
-		pos = get_from_dict(kwargs, 'pos')
+		c = self.get_c(kwargs)
+		pos = self.get_pos(kwargs)
 		r = get_first_from_dict(kwargs, 'r', 'radius')
-		outline = get_from_dict(kwargs, 'outline')
-		scaled = get_from_dict(kwargs, 'scaled', default = False)
+		outline = self.get_outline(kwargs)
+		scaled = self.get_scaled(kwargs)
 
-		if None in (c, pos, r):
-			raise ValueError('Must provide colour, position and size')
 		
-		c = Colour(c)
-		pos = Vector2(pos)
-		outline = Colour(outline) if outline is not None else c
+		self.not_None(colour=c, position=pos, radius=r)
 
-		if not scaled:
-			pos *= self.size
-			r *= min(*self.size.tuple())
-
+		pos, r = self.__scale(pos, r, scaled=scaled)
 		r = int(r)
 
 		draw.filled_circle(
@@ -244,21 +340,14 @@ class Engine():
 		)
 
 	def circle(self, **kwargs):
-		c = get_first_from_dict(kwargs, 'c', 'col', 'colour')
-		pos = get_from_dict(kwargs, 'pos')
+		c = self.get_c(kwargs)
+		pos = self.get_pos(kwargs)
 		r = get_first_from_dict(kwargs, 'r', 'radius')
-		scaled = get_from_dict(kwargs, 'scaled', default = False)
-
-		if None in (c, pos, r):
-			raise ValueError('Must provide colour, position and size')
+		scaled = self.get_scaled(kwargs)
 		
-		c = Colour(c)
-		pos = Vector2(pos)
+		self.not_None(colour=c, position=pos, radius=r)
 
-		if not scaled:
-			pos *= self.size
-			r *= min(*self.size.tuple())
-
+		pos, r = self.__scale(pos, r, scaled=scaled)
 		r = int(r)
 
 		draw.filled_circle(
@@ -268,44 +357,37 @@ class Engine():
 			c
 		)
 
+	def line(self, **kwargs):
+		c = self.get_c(kwargs)
+		a = self.get_Vector2(kwargs, 'a')
+		b = self.get_Vector2(kwargs, 'b')
+		scaled = self.get_scaled(kwargs)
+
+		self.not_None(colour=c, a=a, b=b)
+
+		a, b = self.__scale(a, b, scaled=scaled)
+
+		draw.line(self.display, *a.tuple(int), *b.tuple(int), c)
+
 	def text(self, text, **kwargs):
-		f = get_first_from_dict(kwargs, 'f', 'font')
-		c = get_first_from_dict(kwargs, 'c', 'col', 'colour')
-		bg = get_first_from_dict(kwargs, 'bg', 'background')
-		pos = get_from_dict(kwargs, 'pos')
-		horiz_align = get_from_dict(kwargs, 'horiz_align', default=Engine.CENTERh)
-		vert_align = get_from_dict(kwargs, 'vert_align', default=Engine.CENTERv)
-		align = get_from_dict(kwargs, 'align', default=horiz_align | vert_align)
-		scaled = get_from_dict(kwargs, 'scaled', default=False)
+		pos = self.get_pos(kwargs)
+		align = self.get_alignment(kwargs)
+		scaled = self.get_scaled(kwargs)
 
-		f = Font(f)
-		c = Colour(c)
-		pos = Vector2(pos)
+		pos = self.__scale(pos, scaled=scaled)
 
-		if not scaled:
-			pos *= self.size
-
-		text_surface = f.render(text, True, c, bg)
-		text_rect = text_surface.get_rect()
-
-		s = Vector2(text_rect.size) / 2
-
-		p = pos
-
-		if align & Engine.BOTTOM != 0:
-			pos.y -= s.y
-		elif align & Engine.CENTERv != 0:
-			pass # nothing needs doing
-		elif align & Engine.TOP != 0:
-			pos.y += s.y
-
-		if align & Engine.LEFT != 0:
-			pos.x += s.x
-		elif align & Engine.CENTERh != 0:
-			pass # nothing needs doing
-		elif align & Engine.RIGHT != 0:
-			pos.x -= s.x
-
-		text_rect.center = p.tuple(int)
+		text_surface, text_rect = self.render_text(text, **kwargs)
+		pos += self.align(text_rect.size, align)
+		text_rect.center = pos.tuple(int)
 
 		self.display.blit(text_surface, text_rect)
+
+	def translate(self, xOff, yOff=None):
+		self.translation += Vector2(xOff, yOff)
+
+	def push(self):
+		self.translations.append(self.translation)
+
+	def pop(self):
+		if len(self.translations) > 0:
+			self.translation = self.translations.pop()
